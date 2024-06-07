116115e870f5a064d22cadbcaf3df59070ff3665
iMGamal <mohamed.gamal126027@gmail.com>
2024-06-07 17:51:21 +0000

    task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -18,7 +18,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 		printf("Memory allocation failed\n");
 		return (NULL);
 	}
-	printf("");
+	printf("Enter value");
 	scanf("%d", &value);
 	if (value == -1)
 	{
