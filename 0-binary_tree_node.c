985b6d77a566cde5024c0c12a4e353694ad4e84f
iMGamal <mohamed.gamal126027@gmail.com>
2024-06-07 17:25:30 +0000

    0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,28 +10,25 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *create()
+	int x;
+
+	binary_tree_t *newnode;
+
+	newnode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	if (newnode == NULL)
 	{
-		int x;
-
-		binary_tree_t *newnode;
-
-		newnode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-		if (newnode == NULL)
-		{
-			printf("Memory allocation failed\n");
-			return (NULL);
-		}
-		printf("");
-		scanf("%d", &x);
-		if (x == -1)
-		{
-			free(newnode);
-			return (NULL);
-		}
-		newnode->n = x;
-		newnode->left = create();
-		newnode->right = create();
-		return (newnode);
+		printf("Memory allocation failed\n");
+		return (NULL);
 	}
+	printf("");
+	scanf("%d", &x);
+	if (x == -1)
+	{
+		free(newnode);
+		return (NULL);
+	}
+	newnode->n = x;
+	newnode->left = create();
+	newnode->right = create();
+	return (newnode);
 }
