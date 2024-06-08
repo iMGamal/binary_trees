ca134eb991fccaf2e3e95afdd356a5986d253586
iMGamal <mohamed.gamal126027@gmail.com>
2024-06-08 00:16:32 +0000

    task 3

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -3,7 +3,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_leftt - Inserts a leftt child
+ * binary_tree_insert_left - Inserts a leftt child
  * @parent: Pointer to the parent node
  * @value: Value to be assigned to new node
  * Return: Returns a pointer to node or NULL on failure of if parent is NULL
--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -11,7 +11,6 @@ void binary_tree_delete(binary_tree_t *tree)
 	if (tree != NULL)
 	{
 		binary_tree_delete(tree->parent);
-		binary_tree_delete(tree->n);
 		binary_tree_delete(tree->left);
 		binary_tree_delete(tree->right);
 		free(tree);
