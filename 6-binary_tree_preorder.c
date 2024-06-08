1799e64ba7bb217f98f411b7a3640ffefdd698ad
iMGamal <mohamed.gamal126027@gmail.com>
2024-06-08 02:54:53 +0000

    F

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -3,7 +3,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - 
+ * binary_tree_preorder - Prints element of tree 
  * @tree: Pointer to the tree
  * @func: Pointer to function for node
  */
@@ -13,7 +13,11 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	{
 		return;
 	}
-	binary_tree_preorder(tree->left, func);
-	binary_tree_preorder(tree->right, func);
-	func(tree->n);
+	else
+	{
+
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+		func(tree->n);
+	}
 }
